25314037fe775fcb399f80a217f40ae24f707fad
amathe18 <amathe18@gmail.com>
2024-09-06 09:56:52 -0400

    Add files via upload

---
+++ lab2.js
@@ -0,0 +1,39 @@
+
+for (let x = 1; x <= 100; x++) 
+    if (x % 3 === 0 && x % 5 === 0) {
+      console.log("FizzBuzz");
+    } else if (x % 3 === 0) {
+      console.log("Fizz");
+    } else if (x % 5 === 0) {
+      console.log("Buzz");
+    } else 
+      console.log(x);
+  
+/// part 2//////
+
+let n = 18
+    for (let n = 18; n >= 18; n++) {
+        if (n % 2 === 0);
+    console.log(n);}
+
+
+for (let x = 1; x <= 100; x++) 
+    if (x % 3 === 0 && x % 5 === 0) {
+      console.log("FizzBuzz");
+    } else if (x % 3 === 0) {
+      console.log("Fizz");
+    } else if (x % 5 === 0) {
+      console.log("Buzz");
+    } else 
+      console.log(x);
+  
+/// part 2//////
+
+let x = 18
+    for (let x = 18; x >= 18; x++) {
+        if (x % 2 === 0);
+    console.log(x);}
+
+
+
+//// for whatever reason i cant launch this code , but it is is the first time it hasnt given an error so i hope its correct, im not sure what this "M" is despite all my troubleshooting////
